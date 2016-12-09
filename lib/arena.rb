05ec44bbeb90fe8f78a3e7c9609f8d85d693daf0
murphypicard <ayaghsizian@gmail.com>
2016-12-09 14:33:38 -0500

    working on last 4 fight errors

--- lib/arena.rb
+++ lib/arena.rb
@@ -3,6 +3,30 @@ class Arena
 
   def initialize(name)
     @name = name.capitalize
+    @gladiators = []
+  end
+
+  def gladiators
+    return @gladiators
+  end
+
+  def add_gladiator(him)
+    if @gladiators.length < 2
+      return @gladiators.push(him)
+    end
+  end
+
+  def fight
+    if @gladiators.length == 0
+      return @gladiators
+    elsif @gladiators.length == 1
+      return @gladiators
+    elsif @gladiators.length == 2
+      if @gladiators[1].weapon == "Spear"
+        return @gladiators[1]
+      end
+        
+    end
   end
 
 end
